5abd54011d1549b005ea4e7fab141d47fab6144d
mkvbs <maciej.k-2000@outlook.com>
2024-11-19 20:13:03 +0100

    #1 - Refactor model classes.

--- src/main/java/com/mkvbs/recipe_management_service/model/Ingredient.java
+++ src/main/java/com/mkvbs/recipe_management_service/model/Ingredient.java
@@ -2,14 +2,12 @@ package com.mkvbs.recipe_management_service.model;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
-@Builder(access = AccessLevel.PUBLIC)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Ingredient {
 
     private final UUID id;
