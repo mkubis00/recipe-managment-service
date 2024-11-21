902d7935f810fed9e0db63abb4bba421681c5e51
mkvbs <maciej.k-2000@outlook.com>
2024-11-21 17:08:53 +0100

    #7 - Write test to factories.
    Signed-off-by: mkvbs <maciej.k-2000@outlook.com>

--- src/test/java/com/mkvbs/recipe_management_service/factory/ingredient/IngredientsResponseFactoryTest.java
+++ src/test/java/com/mkvbs/recipe_management_service/factory/ingredient/IngredientsResponseFactoryTest.java
@@ -3,12 +3,11 @@ package com.mkvbs.recipe_management_service.factory.ingredient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class IngredientsResponseFactoryTest {
 
     @BeforeEach
     void setUp() {
+        //
     }
 
     @Test
