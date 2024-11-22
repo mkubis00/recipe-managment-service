04cf84b2689493bce85811d4fc1388b6f80421e8
mkvbs <maciej.k-2000@outlook.com>
2024-11-22 23:26:15 +0100

    #11 - application.properties update
    Signed-off-by: mkvbs <maciej.k-2000@outlook.com>

---
+++ src/test/java/com/mkvbs/recipe_management_service/BasicIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.mkvbs.recipe_management_service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class BasicIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres")
+            .withDatabaseName("test_database")
+            .withUsername("root")
+            .withPassword("root")
+            .withExposedPorts(5435);
+
+    @BeforeAll
+    public static void setUp() {
+        postgres.setPortBindings(List.of("5435:5432"));
+        postgres.start();
+    }
+}
--- src/test/java/com/mkvbs/recipe_management_service/RecipeManagementServiceApplicationTests.java
+++ src/test/java/com/mkvbs/recipe_management_service/RecipeManagementServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.mkvbs.recipe_management_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class RecipeManagementServiceApplicationTests {
 
 	@Test
