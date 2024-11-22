bfd996c32e0ace238879da0973a6eae5782e06d9
mkvbs <maciej.k-2000@outlook.com>
2024-11-22 23:26:15 +0100

    #11 - application.properties update
    Signed-off-by: mkvbs <maciej.k-2000@outlook.com>

--- src/test/java/com/mkvbs/recipe_management_service/RecipeManagementServiceApplicationTests.java
+++ src/test/java/com/mkvbs/recipe_management_service/RecipeManagementServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package com.mkvbs.recipe_management_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class RecipeManagementServiceApplicationTests {
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class RecipeManagementServiceApplicationTests extends BasicIntegrationTest {
 
 	@Test
 	void contextLoads() {
