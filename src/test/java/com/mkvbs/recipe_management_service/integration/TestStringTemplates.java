23492424f7bedee18aaaf9eb61d91ced1b5c9c4f
mkvbs <maciej.k-2000@outlook.com>
2024-12-11 16:59:21 +0100

    Added  unit test for post service.
    Signed-off-by: mkvbs <maciej.k-2000@outlook.com>

---
+++ src/test/java/com/mkvbs/recipe_management_service/integration/TestStringTemplates.java
@@ -0,0 +1,13 @@
+package com.mkvbs.recipe_management_service.integration;
+
+public final class TestStringTemplates {
+
+    public static final String DEFAULT_FAIL_MESSAGE = "Exeption during test: ";
+    public static final String JSON_LENGTH = "$.length()";
+    public static final String JSON_ERROR = "$.error";
+
+    public static final String JSON_ID = "$.id";
+    public static final String JSON_NAME = "$.name";
+    public static final String JSON_TYPE_OF_QUANTITY = "$.typeOfQuantity";
+    public static final String JSON_ALLERGEN = "$.allergen";
+}
