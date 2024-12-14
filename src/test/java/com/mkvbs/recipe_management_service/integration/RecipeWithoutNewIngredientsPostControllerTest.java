63083034d11c2032e325ebcbec7eedb36e7d61f6
mkvbs <maciej.k-2000@outlook.com>
2024-12-14 19:39:14 +0100

    #18 - Add validation for requests.
    Validation was added for classes IngredientRequest, RecipeRequest and RecipeRequestWithNewIngredients. Also new values in ValidationMessage. Correction of some tests was needed as well.

--- src/test/java/com/mkvbs/recipe_management_service/integration/RecipeWithoutNewIngredientsPostControllerTest.java
+++ src/test/java/com/mkvbs/recipe_management_service/integration/RecipeWithoutNewIngredientsPostControllerTest.java
@@ -9,7 +9,7 @@ import com.mkvbs.recipe_management_service.model.api.recipe.request.RecipeReques
 import com.mkvbs.recipe_management_service.proxy.IngredientManagementProxy;
 import com.mkvbs.recipe_management_service.resource.api.ApiPath;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -29,8 +29,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class RecipeWithoutNewIngredientsPostControllerTest extends BasicIntegration {
 
-    @MockBean
-    private ResponseEntity<Ingredient> ingredientResponseEntity;
+    @Mock
+    private ResponseEntity<Ingredient> firstIngredientResponseEntity;
+    @Mock
+    private ResponseEntity<Ingredient> secondIngredientResponseEntity;
+    @Mock
+    private ResponseEntity<Ingredient> thirdIngredientResponseEntity;
+
     @MockBean
     private IngredientManagementProxy ingredientManagementProxy;
 
@@ -39,11 +44,15 @@ public class RecipeWithoutNewIngredientsPostControllerTest extends BasicIntegrat
     private final String recipeName = "recipeName";
     private final String recipeDescription = "recipeDescriptionaaaaaaaaaaaaaaaaaaaa";
     private final MealType mealType = MealType.DESSERT;
-    private final List<String> pictureLinks = List.of("link1", "link2");
-    private final List<String> steps = List.of("step1", "step2");
+    private final List<String> pictureLinks = List.of("link1", "link2", "link3");
+    private final List<String> steps = List.of("step1", "step2", "step3");
 
-    private Ingredient ingredient;
-    private UUID ingredientId;
+    private Ingredient firstIngredient;
+    private Ingredient secondIngredient;
+    private Ingredient thirdIngredient;
+    private UUID firstIngredientId;
+    private UUID secondIngredientId;
+    private UUID thirdIngredientId;
     private final String ingredientName = "ingredientName";
     private final TypeOfQuantity typeOfQuantity = TypeOfQuantity.NONE;
     private final Allergen allergen = Allergen.NONE;
@@ -52,21 +61,38 @@ public class RecipeWithoutNewIngredientsPostControllerTest extends BasicIntegrat
     private ObjectMapper objectMapper;
 
     public RecipeWithoutNewIngredientsPostControllerTest() {
-        this.ingredientId = UUID.randomUUID();
-        this.ingredient = new Ingredient(ingredientId, ingredientName, 1, typeOfQuantity, allergen);
+        this.firstIngredientId = UUID.randomUUID();
+        this.secondIngredientId = UUID.randomUUID();
+        this.thirdIngredientId = UUID.randomUUID();
+
+        this.firstIngredient = new Ingredient(firstIngredientId, ingredientName, 1, typeOfQuantity, allergen);
+        this.secondIngredient = new Ingredient(secondIngredientId, ingredientName, 2, typeOfQuantity, allergen);
+        this.thirdIngredient = new Ingredient(thirdIngredientId, ingredientName, 3, typeOfQuantity, allergen);
     }
 
     @Test
     void testPostRecipeWithoutNewIngredients() throws Exception {
-        when(ingredientResponseEntity.getBody()).thenReturn(ingredient);
-        when(ingredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+        when(firstIngredientResponseEntity.getBody()).thenReturn(firstIngredient);
+        when(firstIngredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
 
-        request = new RecipeRequestWithoutNewIngredients(recipeName, recipeDescription, mealType, pictureLinks, steps, Map.of(ingredientId, 2.0));
+        when(secondIngredientResponseEntity.getBody()).thenReturn(secondIngredient);
+        when(secondIngredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+
+        when(thirdIngredientResponseEntity.getBody()).thenReturn(thirdIngredient);
+        when(thirdIngredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+
+        request = new RecipeRequestWithoutNewIngredients(recipeName, recipeDescription, mealType, pictureLinks, steps, Map.of(firstIngredientId, 2.0, secondIngredientId, 3.0, thirdIngredientId, 4.0));
         String jsonRecipe = objectMapper.writeValueAsString(request);
 
 
-        when(ingredientManagementProxy.getIngredientById(ingredientId))
-                .thenReturn(ingredientResponseEntity);
+        when(ingredientManagementProxy.getIngredientById(firstIngredientId))
+                .thenReturn(firstIngredientResponseEntity);
+
+        when(ingredientManagementProxy.getIngredientById(secondIngredientId))
+                .thenReturn(secondIngredientResponseEntity);
+
+        when(ingredientManagementProxy.getIngredientById(thirdIngredientId))
+                .thenReturn(thirdIngredientResponseEntity);
 
         mockMvc.perform(post(ApiPath.RECIPE.CREATE_V1)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -76,31 +102,43 @@ public class RecipeWithoutNewIngredientsPostControllerTest extends BasicIntegrat
                 .andExpect(jsonPath("$.name").value(recipeName))
                 .andExpect(jsonPath("$.description").value(recipeDescription))
                 .andExpect(jsonPath("$.mealType").value(mealType.toString()))
-                .andExpect(jsonPath("$.picturesLinks", hasSize(2)))
+                .andExpect(jsonPath("$.picturesLinks", hasSize(3)))
                 .andExpect(jsonPath("$.picturesLinks", hasItem(pictureLinks.get(0))))
                 .andExpect(jsonPath("$.picturesLinks", hasItem(pictureLinks.get(1))))
-                .andExpect(jsonPath("$.recipeSteps", hasSize(2)))
+                .andExpect(jsonPath("$.recipeSteps", hasSize(3)))
                 .andExpect(jsonPath("$.recipeSteps", hasItem(steps.get(0))))
                 .andExpect(jsonPath("$.recipeSteps", hasItem(steps.get(1))))
-                .andExpect(jsonPath("$.ingredients", hasSize(1)))
-                .andExpect(jsonPath("$.ingredients[0].id").value(ingredientId.toString()))
+                .andExpect(jsonPath("$.ingredients", hasSize(3)))
+                .andExpect(jsonPath("$.ingredients[0].id").value(firstIngredientId.toString()))
                 .andExpect(jsonPath("$.ingredients[0].name").value(ingredientName))
                 .andExpect(jsonPath("$.ingredients[0].quantity").value(2.0));
     }
 
     @Test
     void testPostRecipeWithNotExistingIngredient() throws Exception {
-        when(ingredientManagementProxy.getIngredientById(ingredientId))
+        when(ingredientManagementProxy.getIngredientById(firstIngredientId))
                 .thenReturn(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 
-        request = new RecipeRequestWithoutNewIngredients(recipeName, recipeDescription, mealType, pictureLinks, steps, Map.of(ingredientId, 2.0));
+        when(secondIngredientResponseEntity.getBody()).thenReturn(secondIngredient);
+        when(secondIngredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+
+        when(thirdIngredientResponseEntity.getBody()).thenReturn(thirdIngredient);
+        when(thirdIngredientResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+
+        when(ingredientManagementProxy.getIngredientById(secondIngredientId))
+                .thenReturn(secondIngredientResponseEntity);
+
+        when(ingredientManagementProxy.getIngredientById(thirdIngredientId))
+                .thenReturn(thirdIngredientResponseEntity);
+
+        request = new RecipeRequestWithoutNewIngredients(recipeName, recipeDescription, mealType, pictureLinks, steps, Map.of(firstIngredientId, 2.0, secondIngredientId, 3.0, thirdIngredientId, 4.0));
         String jsonRecipe = objectMapper.writeValueAsString(request);
 
         mockMvc.perform(post(ApiPath.RECIPE.CREATE_V1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRecipe))
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath(TestStringTemplates.JSON_ERROR).value("Ingredient with id " + ingredientId + " not found."));
+                .andExpect(jsonPath(TestStringTemplates.JSON_ERROR).value("Ingredient with id " + firstIngredientId + " not found."));
     }
 
 }
