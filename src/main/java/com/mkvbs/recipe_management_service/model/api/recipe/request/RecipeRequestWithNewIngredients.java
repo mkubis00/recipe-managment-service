203f9021c0d8698fd771f1932926c31ee4560636
mkvbs <maciej.k-2000@outlook.com>
2024-12-14 19:39:14 +0100

    #18 - Add validation for requests.
    Validation was added for classes IngredientRequest, RecipeRequest and RecipeRequestWithNewIngredients. Also new values in ValidationMessage. Correction of some tests was needed as well.

--- src/main/java/com/mkvbs/recipe_management_service/model/api/recipe/request/RecipeRequestWithNewIngredients.java
+++ src/main/java/com/mkvbs/recipe_management_service/model/api/recipe/request/RecipeRequestWithNewIngredients.java
@@ -2,6 +2,9 @@ package com.mkvbs.recipe_management_service.model.api.recipe.request;
 
 import com.mkvbs.recipe_management_service.model.MealType;
 import com.mkvbs.recipe_management_service.model.api.ingredient.IngredientRequest;
+import com.mkvbs.recipe_management_service.resource.api.ValidationMessage;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.util.List;
@@ -11,6 +14,8 @@ import java.util.UUID;
 @Getter
 public class RecipeRequestWithNewIngredients extends RecipeRequest {
 
+    @Size(min = 2, message = ValidationMessage.NEW_INGREDIENTS_LEN)
+    @NotNull(message = ValidationMessage.NEW_INGREDIENTS_NOT_NULL)
     private final List<IngredientRequest> newIngredients;
 
     public RecipeRequestWithNewIngredients(String name, String description, MealType mealType, List<String> picturesLinks, List<String> recipeSteps, Map<UUID, Double> ingredients, List<IngredientRequest> newIngredients) {
--- src/main/java/com/mkvbs/recipe_management_service/resource/api/ValidationMessage.java
+++ src/main/java/com/mkvbs/recipe_management_service/resource/api/ValidationMessage.java
@@ -8,7 +8,29 @@ public class ValidationMessage {
 
     public static final String INCORRECT_NAME_LEN = "name must be between 3 and 50 characters";
     public static final String NAME_NOT_NULL = "name cannot be null";
+
     public static final String DESCRIPTION_NOT_NULL = "description cannot be null";
     public static final String INCORRECT_DESCRIPTION_LEN = "name must be between 20 and 200 characters";
+
+    public static final String MEAL_TYPE_NOT_NULL = "meal type cannot be null";
+
+    public static final String PICTURES_LINKS_LEN = "pictures links length should be greater than 1 ";
+    public static final String PICTURES_LINKS_NOT_NULL = "pictures links cannot be null";
+
+    public static final String RECIPE_STEPS_LEN = "recipe steps length should be greater than 2";
+    public static final String RECIPE_STEPS_NOT_NULL = "recipe steps cannot be null";
+
+    public static final String INGREDIENTS_LEN = "ingredients length should be greater than 2";
+    public static final String INGREDIENTS_NOT_NULL = "ingredients cannot be null";
+
+    public static final String NEW_INGREDIENTS_LEN = "newIngredients length should be greater than 1";
+    public static final String NEW_INGREDIENTS_NOT_NULL = "newIngredients cannot be null";
+
+    public static final String TYPE_OF_QUA_NOT_NULL = "type of quantity cannot be null";
+    public static final String ALLERGEN_NOT_NULL = "type of allergen cannot be null";
+
+    public static final String QUANTITY_VALUE = "quantity value should be greater than 0.1";
+    public static final String QUANTITY_NOT_NULL = "quantity cannot be null";
+
     public static final String HTTP_MESSAGE_NOT_READABLE = "Some of the values aren't assigned correctly. Check structure of the json.";
 }
