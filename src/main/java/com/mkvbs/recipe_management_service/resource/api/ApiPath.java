c840362f6db4fc1b3b24d112be99287421f88629
mkvbs <maciej.k-2000@outlook.com>
2024-12-14 21:05:16 +0100

    #21 - Take care of resource api path class

--- src/main/java/com/mkvbs/recipe_management_service/resource/api/ApiPath.java
+++ src/main/java/com/mkvbs/recipe_management_service/resource/api/ApiPath.java
@@ -26,20 +26,5 @@ public class ApiPath {
         public static final String GET_ALL_V1 = BASE_V1 + "/all";
 
         public static final String TEST_V1 = BASE_V1 + "/";
-        public static final String TEST_GET_FROM_ID_LIST_V1 = BASE_V1 + "/from_id_list/";
-        public static final String TEST_GET_INGREDIENT_ID_BY_NAME_V1 = BASE_V1 + "/id_by_name/";
-        public static final String TEST_GET_LIST_WITH_SIMILAR_NAME_V1 = BASE_V1 + "/with_similar_name/";
-    }
-
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class ALLERGEN {
-
-        public static final String BASE_V1 = API_VERSION_V1 + "/allergens";
-
-        public static final String GET_DESCRIPTION_V1 = BASE_V1 + "/description/{allergen}";
-        public static final String GET_FROM_INGREDIENTS_IDS_V1 = BASE_V1 + "/from_ingredients_ids/{ingredientsIds}";
-
-        public static final String TEST_GET_DESCRIPTION_V1 = BASE_V1 + "/description/";
-        public static final String TEST_GET_FROM_INGREDIENTS_IDS_V1 = BASE_V1 + "/from_ingredients_ids/";
     }
 }
